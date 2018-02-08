5e82d875b99cb3d81a27d64132e6cd5fb94967df
Pedro Roldan <pedro.roldan@gmail.com>
2018-02-08 14:46:11 +0000

    db added

--- monitor.py
+++ monitor.py
@@ -1,16 +1,27 @@
 import serial
+import mysql.connector
 import time
+# from time import time, gmtime, strftime
+from datetime import datetime
 import serial.tools.list_ports
 
 ARDUINO = "/dev/ttyUSB0"
 BAUDRATE = 9600
 TIMEOUT = 1
 arduinoConection = None
-realpower = 0.0
+realPower = 0.0
 
 # ports = list(serial.tools.list_ports.comports())
 # for p in ports:
 # 	print p.description + p.device
+
+# conn = mysql.connector.Connect(host='127.0.0.1', user='monitor', password='monitor', database='monitor')
+# c = conn.cursor()
+#
+# c.execute("SELECT * from readings")
+# row = c.fetchone()
+# print row[4]
+# c.close()
 
 # abertura de porta serie
 try:
@@ -23,19 +34,38 @@ except:
 try:
 	if arduinoConection.isOpen():  # ligacao aberta
 
+		conn = mysql.connector.Connect(host='127.0.0.1', user='monitor', password='monitor', database='monitor')
+		c = conn.cursor()
+
 		while arduinoConection.in_waiting:
-			time.sleep(2)
 			arduinoData = arduinoConection.readline()
-			# arduinoData = arduinoConection.readline()
 
 			pieces = arduinoData.split("\t")
-			realpower = pieces[0]
-			apparentPower = pieces[1]
 
-			# print "realPower: " + realpower + " Apparent Power: " + apparentPower
+			timestamps = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+			node = 1
+			ct = 1
+			realPower = float(pieces[0][pieces[0].index(":")+1:])
+			apparentPower = float(pieces[1][pieces[1].index(":")+1:])
+			supplyVoltage = float(pieces[2][pieces[2].index(":")+1:])
+			irms = float(pieces[3][pieces[3].index(":")+1:])
+			powerFactor = float(pieces[4][pieces[4].index(":")+1:])
+
+			addReading = ("INSERT INTO readings "
+							"(timestamp, node, ct, rpower, apower, svoltage, irms, pfactor) "
+							"VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+			dataReading = (timestamps, node, ct, realPower, apparentPower, supplyVoltage, irms, powerFactor)
+
+			c.execute(addReading, dataReading)
+
+			conn.commit()
+
 			print(arduinoData)
+			time.sleep(5)  # tempo de espera 5 segundos
 
 		arduinoConection.close
+		c.close()
+		conn.close()
 
 except KeyboardInterrupt:  # captura termino ctrl+c
 	arduinoConection.close
